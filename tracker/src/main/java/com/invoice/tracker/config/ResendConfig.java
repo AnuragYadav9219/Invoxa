b90eb621ede373b55f356248c2699004cff82e51
Lucifer <anuragyad1398@gmail.com>
2026-07-17 21:29:00 +0530

    Fixed email configurations

--- tracker/src/main/java/com/invoice/tracker/config/ResendConfig.java
+++ tracker/src/main/java/com/invoice/tracker/config/ResendConfig.java
@@ -3,19 +3,26 @@ package com.invoice.tracker.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
 import org.springframework.web.client.RestClient;
 
+import lombok.extern.slf4j.Slf4j;
+
 @Configuration
+@Slf4j
 public class ResendConfig {
 
     @Bean
-    RestClient resendRestClient(
-            @Value("${resend.api.key") String apiKey) {
+    public RestClient resendRestClient(
+            @Value("${resend.api.key}") String apiKey) {
+
+        log.info("API Key starts with: {}", apiKey.substring(0, 5));
+        log.info("API Key length: {}", apiKey.length());
 
         return RestClient.builder()
                 .baseUrl("https://api.resend.com")
-                .defaultHeader("Authorization", "Bearer" + apiKey)
-                .defaultHeader("Content-Type", "application/json")
+                .defaultHeader(HttpHeaders.AUTHORIZATION, "Bearer " + apiKey)
+                .defaultHeader(HttpHeaders.CONTENT_TYPE, "application/json")
                 .build();
     }
 }
